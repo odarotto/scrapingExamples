d13fa8099549fbfdf7df67808da2ba3e2eab78f9
Rev Io <odarotto@gmail.com>
2018-11-05 21:58:23 -0400

    Optimizing image scraping and adding video download

--- scrapingIGProfile/igScraping.py
+++ scrapingIGProfile/igScraping.py
@@ -3,6 +3,7 @@ import re
 import subprocess
 import requests
 import shutil
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
@@ -15,6 +16,11 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+# TODO 
+# Optimize image scraping
+# Add command line utilities
+# Private profile scraping
+# Video downloads
 
 def initializeDriver(url):
     '''gets an URL and initialize a driver open and loaded'''
@@ -48,12 +54,11 @@ def isPrivate(driver, url):
         print('[*] Public Account')
         return False
 
-def scrapePostsLinks(driver, url):
+def scrapePosts(driver, url):
     '''Scrape the links for the posts in the account'''
-    links = driver.find_elements_by_xpath(
-        '//a[starts-with(@href, "/p/")]')
+    links = driver.find_elements_by_xpath('//a[starts-with(@href, "/p/")]')
     urls = set()
-    reachBottom = driver.execute_script(
+    driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight);")
     limit = int(driver.find_element_by_class_name('g47SY ').text)
 
@@ -62,18 +67,31 @@ def scrapePostsLinks(driver, url):
             '//a[starts-with(@href, "/p/")]')
         for link in links:
             try:
-                if link.get_attribute('href') not in urls:
-                    urls.add(link.get_attribute('href'))
-                    print('{} added'.format(link.get_attribute('href')))
+                divs = link.find_elements_by_tag_name('div')
+                media_link = ''
+                if len(divs) == 5:
+                    media_link = link.get_attribute('href')
+                    t = ('V', media_link)
+                    if t not in urls:
+                        urls.add(t)
+                        print('{} added'.format(media_link))
+                else:
+                    media_link = link.find_element_by_class_name('KL4Bh')\
+                        .find_element_by_tag_name('img')
+                    t = ('I', media_link.get_attribute('src'))
+                    if t not in urls:
+                        urls.add(t)
+                        print('{} added'.format(media_link.get_attribute('src')\
+                            .split('/')[-1]))
             except StaleElementReferenceException:
-                reachBottom = driver.execute_script(
+                driver.execute_script(
                     "window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
         reachBottom = driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
     return urls
 
-def downloadPosts(driver, urls):
+def downloadContent(driver, urls):
     '''Download the content from the urls according with the
     download option.'''
 
@@ -81,32 +99,60 @@ def downloadPosts(driver, urls):
     download_option = input('[!] Do you want the links in a .txt file or want'\
     'me to download the images for you?(f:file, l:links):')
 
+    print('[!] Sorting URLs')
+    urls = sorted(urls)
+    image_urls = [url for url in urls if url[0]=='I']
+    video_posts = [url for url in urls if url[0]=='V']
+
     if download_option == 'f':
         # Scrape image's links and download images
-        for url in urls:
-            driver.get(url)
-            time.sleep(2)
-            media_url = ''
-            try:
-                media_url = driver.find_element_by_tag_name('article')\
-                    .find_element_by_class_name('KL4Bh')\
-                    .find_element_by_tag_name('img').get_attribute('src')
-            except NoSuchElementException:
-                # If no image, then the media is a video
-                continue
-            r = requests.get(media_url, stream=True)
-            filename = ''
-            if r.status_code == 200:
-                filename = media_url.split('/')[-1]
-                with open(filename, 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-            print('{} saved'.format(filename))
+        # for url in image_urls:
+        #     downloadImage(url)
+        for url in video_posts:
+            downloadVideo(driver, url)
     elif download_option == 'l':
         return urls
     else:
         print('[!] Please enter a valid option')
         pass
+
+def downloadVideo(driver, url):
+    '''Opens a driver and downloads the video from post'''
+    link = url[-1]
+    driver.get(link)
+    time.sleep(3)
+    html = driver.find_element_by_tag_name('html').get_attribute('innerHTML')
+    bs = BeautifulSoup(html, 'lxml')
+
+    video_links = bs.find('article').find('div', {'class':'_97aPb wKWK0'})\
+        .find_all('div')
+    for video in video_links:
+        if video.get('class') == '_5wCQW':
+            # video.find()
+            video_link = video.find('video').get('src')
+    # video_link = driver.find_element_by_class_name('OAXCp ')\
+    #         .find_element_by_tag_name('video').get_attribute('src')
+    r = requests.get(video_link, stream=True)
+    filename = ''
+    if r.status_code == 200:
+        filename = link.split('/')[-1]
+        with open(filename, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+    print('{} saved'.format(filename))
+
+def downloadImage(url):
+    '''Download an image from url'''
+    link = url[-1]
+    r = requests.get(link, stream=True)
+    filename = ''
+    if r.status_code == 200:
+        filename = link.split('/')[-1]
+        with open(filename, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+    print('{} saved'.format(filename))
+
 
 def logIn(driver):
     '''Ask the user for login credentials and make the login'''
@@ -134,12 +180,12 @@ if __name__ == '__main__':
             else:
                 pass
             # Scrape images directly
-            posts_urls = scrapePostsLinks(driver, url)
+            posts_urls = scrapePosts(driver, url)
             pass
         else:
             # Scrape images directly
-            posts_urls = scrapePostsLinks(driver, url)
+            posts_urls = scrapePosts(driver, url)
             pass
-        downloadPosts(driver, posts_urls)
+        downloadContent(driver, posts_urls)
     finally:
         driver.close()
